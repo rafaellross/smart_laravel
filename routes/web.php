b36a72d7c5d2d8ee23ba3821a200b82cf87f22f2
Rafael Ross <rafaellross@gmail.com>
2018-05-02 11:15:03 +1000

    Improving cache

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Config::set('debugbar.enabled', false);
+//Config::set('debugbar.enabled', false);
 Route::get('/', function () {
     return view('welcome');
 });
