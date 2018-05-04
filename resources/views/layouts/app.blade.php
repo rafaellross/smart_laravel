c61c5ddacac4649243aad240198e643f9741d35a
Rafael Ross <rafaellross@gmail.com>
2018-05-04 13:47:34 +1000

    optimize speed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="Cache-control" content="public">
+    <meta http-equiv="Expires" content="30">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <link rel="shortcut icon" href="{{{ asset('img/brand.ico') }}}">
     <link rel="apple-touch-icon" href="{{ asset('img/brand.ico') }}">  
