8057fb4e65a7f1b0b574629bb37074cf44c826e6
Rafael Ross <rafaellross@gmail.com>
2018-08-01 11:20:04 +1000

    Fixed assets

--- app/LabelPenetration.php
+++ app/LabelPenetration.php
@@ -18,7 +18,7 @@ private $font = ["header" => 15, "label" => 10, "field" => 9, "values" => 7];
   private $title = 'SERVICES PENETRATION FIRE SEAL';
 
 
-  protected $y = 0;
+
 
 
   protected $currentX = 4;
@@ -29,14 +29,6 @@ private $font = ["header" => 15, "label" => 10, "field" => 9, "values" => 7];
 
 
   private function front($fire_identification, $ln = 0) {
-
-    if($this->labelNumber == 0){
-
-      $this->AddPage('L');
-      $this->currentX = 4;
-      $this->currentY = 0;
-
-    }
 
     $rowBreak = 9;
 
@@ -105,63 +97,12 @@ private $font = ["header" => 15, "label" => 10, "field" => 9, "values" => 7];
       case 4:
         $this->currentX = 4;
         $this->currentY = 0;
-        $this->labelNumber = 0;
+        $this->labelNumber = 1;
+        $this->AddPage('L');
 
         break;
 
     }
-
-
-    /*
-    //Check if horizontal position is 2
-    if($this->currentX == $this->width) {
-      $this->currentY = $this->height;
-    }
-
-    //
-    if($this->currentX == 0) {
-      $this->currentX = $this->width;
-
-    }*/
-
-
-    //$this->Cell($this->width , 10, 'Fire Seal Reference:' . $fire_identification->fire_seal_ref, 'BLR', 1, 'L');
-
-    /*
-    $this->Cell($this->width , 10, $this->title, 'LTR', 1, 'C');
-    $this->Text($this->startX, $this->startY, $this->title);
-
-    $this->Cell($this->width , 10, 'DO NOT DISTURB', 'LR', 1, 'C');
-    $this->SetFont('Arial','B', $this->font["label"]);
-    $this->Cell($this->width , 10, 'This fire seal is compliant with the requirements of: AS4072.1, AS1530.4', 'LR', 1, 'C');
-
-    $this->SetFont('Arial','B', $this->font["label"]);
-    $this->Cell($this->width , 10, 'Fire Seal Reference:' . $fire_identification->fire_seal_ref, 'BLR', 1, 'L');
-    $this->Cell($this->width , 10, 'Fire Resistance Level (FRL):' . $fire_identification->fire_resist_level, 'LR', 1, 'L');
-    $this->Cell($this->width , 10, 'Installed By:' . $fire_identification->install_by, 'LR', 1, 'L');
-    $this->Cell($this->width , 10, 'Installation Date:' . Carbon::parse($fire_identification->install_dt)->format('d/m/Y'), 'LR', 1, 'L');
-    $this->Cell($this->width , 10, 'Manufacturer of Fire Stopping System:' . $fire_identification->manufacturer, 'LRB', 1, 'L');
-
-    $qr_code = new QR_Text(
-        "{
-            \"id\": $fire_identification->id,
-            \"project\": \"$fire_identification->description\",
-            \"fire_number\": \"$fire_identification->fire_number\",
-            \"fire_seal_ref\" :  \"$fire_identification->fire_seal_ref\",
-            \"fire_resist_level\" :  \"$fire_identification->fire_resist_level\",
-            \"manufacturer\" :  \"$fire_identification->manufacturer\"
-
-        }");
-
-
-    $qr_code->setMargin(0);
-    $qr_code->setOutfile('tmp/qr_fire_' . $fire_identification->id .'.png')->png();
-
-    $this->Image('tmp/qr_fire_' . $fire_identification->id .'.png', 113, $this->y-37, 30);
-    $this->Text(109, $this->y-3,'Penetration Number: ' . $fire_identification->fire_number);
-
-    */
-
 
 
   }
