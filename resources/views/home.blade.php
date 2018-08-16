896ea14583c7fe84ed0a7ae47d5b41f03f3bbbb0
Rafael Ross <rafaellross@gmail.com>
2018-08-16 16:50:20 +1000

    update home template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,10 @@
                 <div class="card-header">Dashboard</div>
                 <div class="card-body">
                     <a href="{{ URL::to('/timesheets') }}?filter=1" class="list-group-item list-group-item-action ">Time Sheet</a>
+                    <a href="{{ URL::to('/employee_application') }}" class="list-group-item list-group-item-action ">Employee Application</a>
                     @if (isset(Auth::user()->tester) && Auth::user()->tester)
                         <a href="{{ URL::to('/qa_users') }}" class="list-group-item list-group-item-action ">Q.A Forms</a>
-                        <a href="{{ URL::to('/employee_application') }}" class="list-group-item list-group-item-action ">Employee Application</a>
+
                         <a href="{{ URL::to('/form_prestart') }}" class="list-group-item list-group-item-action ">Prestart</a>
                         <a href="{{ URL::to('/form_checklist') }}" class="list-group-item list-group-item-action ">Daily Plant Inspection Checklist</a>
                         <a href="{{ URL::to('/form_service_sheet') }}" class="list-group-item list-group-item-action ">Service Sheet</a>
