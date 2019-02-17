e59bc86646ee6d7ab8a550dc291e7330e1d807f4
Rafael Ross <rafaellross@gmail.com>
2019-02-18 07:50:13 +1100

    released annual leave form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
                 <div class="card-body">
                     <a href="{{ URL::to('/timesheets') }}?filter=1" class="list-group-item list-group-item-action ">Time Sheet</a>
                     <a href="{{ URL::to('/employee_application') }}" class="list-group-item list-group-item-action ">Employee Application</a>
+                    <a href="{{ URL::to('/annual_leave') }}" class="list-group-item list-group-item-action ">Annual Leave Request</a>
                     @if (isset(Auth::user()->tester) && Auth::user()->tester)
                         <a href="{{ URL::to('/qa_users') }}" class="list-group-item list-group-item-action ">Q.A Forms</a>
 
@@ -17,7 +18,7 @@
                         <a href="{{ URL::to('/form_service_sheet') }}" class="list-group-item list-group-item-action ">Service Sheet</a>
                         <a href="{{ URL::to('/fire_identification/scan') }}" class="list-group-item list-group-item-action ">Scan Penetration Tag</a>
                         <a href="{{ env('API_OAUTH_URL', '') . '?client_id=' . env('API_KEY', '') . '&redirect_uri=' . env('API_CALLBACK', '') . '&response_type=code&scope=CompanyFile' }}" class="list-group-item list-group-item-action ">MYOB Integration</a>
-                        <a href="{{ URL::to('/annual_leave') }}" class="list-group-item list-group-item-action ">Annual Leave Request</a>
+
                     @endif
                 </div>
             </div>
