a2a837835bfced71dec46d1c738a8a404c62a53d
Rafael Ross <rafaellross@gmail.com>
2019-04-01 14:19:21 +1100

    fixed travel for rdo during the week

--- app/Day.php
+++ app/Day.php
@@ -30,21 +30,12 @@ class Day extends Model
 
     public function work(){
         $work = false;
-        $deductCodes = array("sick", "anl", "pld", "tafe", "holiday");
+        $deductCodes = array("sick", "anl", "pld", "rdo", "tafe", "holiday");
 
         foreach ($this->dayJobs as $job) {
 
-            if ((isset($job->job->code)) && !in_array($job->job->code, $deductCodes)) {
-                if ($job->job->code == "rdo" && $job->hours() < (6*60)) {
-
-                    $work = false;
-
-                } else {
-
-                    $work = true;
-
-                }
-
+            if ((isset($job->job->code)) && !in_array($job->job->code, $deductCodes) || ( $job->job->code == "rdo" && $job->hours() > (6*60)) ) {
+                $work = true;
             }
         }
         return $work;
